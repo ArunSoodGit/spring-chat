cc85535b60ee7e3b7ecbe0ddd8ca058656baf886
Arun <arun.sood1998@gmail.com>
2020-06-14 18:02:29 +0200

    Init

---
+++ src/main/resources/static/js.js
@@ -0,0 +1,31 @@
+var client = null;
+
+function showMessage(value, user) {
+
+    var newResponse = document.createElement('p');
+    newResponse.appendChild(document.createTextNode(user));
+    newResponse.appendChild(document.createTextNode(": "));
+
+    newResponse.appendChild(document.createTextNode(value));
+
+    var response = document.getElementById('response');
+    response.appendChild(newResponse);
+}
+
+function connect() {
+    client = Stomp.client('ws://localhost:8080/chat');
+    client.connect({}, function (frame) {
+        client.subscribe("/topic/messages", function (message) {
+            showMessage(JSON.parse(message.body).value, JSON.parse(message.body).user)
+
+        });
+    })
+
+}
+
+function sendMessage() {
+    var messageToSent = document.getElementById('messageToSend').value;
+    var user = document.getElementById('user').value;
+    client.send("/app/chat", {}, JSON.stringify({'value': messageToSent, 'user': user}));
+
+}
